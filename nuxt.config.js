33107a834a5816b2d6f7eade63cb39904c00cded
feyifoluwa <abioyefeyifoluwa@gmail.com>
2021-02-16 10:56:19 +0100

    dd cors-anywhere

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,9 +45,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
-  target: 'static',
-  router: {
-    base: '/hubjobsapp/'
-  }
+  // target: 'static',
+  // router: {
+  //   base: '/hubjobsapp/'
+  // }
   
 }
